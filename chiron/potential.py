c343c543cc724f06323b318192af37f9ac042b4e
wiederm <marcus.wieder@gmail.com>
2023-11-21 15:14:59 +0100

    small modifications

--- chiron/potential.py
+++ chiron/potential.py
@@ -29,7 +29,7 @@ class NeuralNetworkPotential:
         return pairlist
 
 
-class LJPotential:
+class LJPotential(NeuralNetworkPotential):
     def __init__(self, sigma, epsilon, topology: Topology):
         self.sigma = sigma  # The distance at which the potential is zero
         self.epsilon = epsilon  # The depth of the potential well
@@ -57,15 +57,16 @@ class LJPotential:
 
 
 class HarmonicOscillatorPotential(NeuralNetworkPotential):
-    def __init__(self, k, x0, U0):
+    def __init__(self, topology, k, x0, U0):
+        self.topology = topology
         self.k = k  # spring constant
         self.x0 = x0  # equilibrium position
         self.U0 = U0  # offset potential energy
 
     def compute_energy(self, positions):
-        # the functional form is given by U(x) = (K/2) * ( (x-x0)^2 + y^2 + z^2 ) + U0 
+        # the functional form is given by U(x) = (K/2) * ( (x-x0)^2 + y^2 + z^2 ) + U0
         # https://github.com/choderalab/openmmtools/blob/main/openmmtools/testsystems.py#L695
-        
+
         # compute the displacement vectors
         displacement_vectors = positions - self.x0
         # Uue the 3D harmonic oscillator potential to compute the potential energy
