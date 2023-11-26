1f31e2f4761576214c5591fc507b9ffbef980993
wiederm <marcus.wieder@gmail.com>
2023-11-26 18:07:57 +0100

    changing basic design

--- chiron/mcmc.py
+++ chiron/mcmc.py
@@ -1,28 +1,63 @@
+"""Markov chain Monte Carlo simulation framework.
+
+This module provides a framework for equilibrium sampling from a given
+thermodynamic state of a biomolecule using a Markov chain Monte Carlo scheme.
+
+It currently offer supports for
+* Langevin dynamics,
+* Monte Carlo,
+* Monte Carlo barostat moves,
+
+which can be combined through the SequenceMove classes.
+
+>>> from chiron import unit
+>>> from openmmtools.testsystems import AlanineDipeptideVacuum
+>>> from chiron.states import StateVariablesCollection
+>>> from chiron.potential import NeuralNetworkPotential
+>>> from modelforge.potential.pretrained_models import SchNetModel
+>>> from chiron.mcmc import MCMCSampler, SequenceMove, MCMove, LangevinDynamicsMove
+ 
+Create the initial state for an alanine
+dipeptide system in vacuum.
+
+>>> alanine_dipeptide = AlanineDipeptideVacuum()
+>>> potential = NeuralNetworkPotential(SchNetModel, alanine_dipeptide.topology)
+>>> state = StateVariablesCollection(temperature=298*unit.kelvin, positions=test.positions)
+
+Create an MCMC move to sample the equilibrium distribution.
+
+>>> langevin_move = LangevinDynamicsMove(n_steps=10)
+
+>>> mc_move = MCMove(timestep=1.0*unit.femtosecond, n_steps=50)
+>>> sampler = MCMCSampler(state, move=ghmc_move)
+
+You can combine them to form a sequence of moves
+
+>>> sequence_move = SequenceMove([ghmc_move, langevin_move])
+>>> sampler = MCMCSampler(thermodynamic_state, sampler_state, move=sequence_move)
+
+"""
+
+
 class GibbsSampler(object):
     """Basic Markov chain Monte Carlo Gibbs sampler.
 
     Parameters
     ----------
-    conditional_state : states.ConditionalDistribution
-        Initial conditional distribution.
+    StateVariablesCollection : states.StateVariablesCollection
+        Defines the states describing the conditional distributions.
     move_set : container of MarkovChainMonteCarloMove objects
-        Moves to attempt during MCMC run. If list or tuple, will run all moves each
-        iteration in specified sequence (e.g. [move1, move2, move3]). If dict, will
-        use specified unnormalized weights (e.g. { move1 : 0.3, move2 : 0.5, move3, 0.9 })
-
-    Examples
-    --------
-
-    Create and run an alanine dipeptide simulation with a weighted move.
+        Moves to attempt during MCMC run.
+        The move set can be a single move or a sequence of moves.
+        The moves will define the joint distributions that are sampled.
 
     """
 
-
-    def __init__(self, conditional_state, move_set):
+    def __init__(self, state_variables, move_set):
         from copy import deepcopy
 
         # Make a deep copy of the state so that initial state is unchanged.
-        self.conditional_state = deepcopy(conditional_state)
+        self.state_variables = deepcopy(state_variables)
         self.move = move_set
 
     def run(self, n_iterations=1):
--- chiron/states.py
+++ chiron/states.py
@@ -1,12 +1,29 @@
+from openmm import unit
 
-class JointDistribution(object):
+
+class StateVariablesCollection:
     """
-    Defines the extended ensamble build from ConditionalDistribution objects.
+    Defines all variables that are necessary to construct a joint distribution of
+    individual conditional distributions that should be sampled.
+    Keeps these variables updated as they change during simulation
+
     """
 
-class ConditionalDistribution(object):
-    """D
+    def __init__(self) -> None:
+        # initialize all state variables
+        self.temperature: unit.Quantity
+        self.volumn: unit.Quantity
+        self.pressure: unit.Quantity
+        self.nr_of_particles: int
+        self.position: unit.Quantity
+        
 
-    Args:
-        object (_type_): _description_
-    """
+    @property
+    def kb(self):
+        return unit.BOLTZMANN_CONSTANT_kB * unit.AVOGADRO_CONSTANT_NA
+
+    @property
+    def beta(self):
+        """Thermodynamic beta in units of mole/energy."""
+        return 1.0 / (self.kb * self.temperature)
+
