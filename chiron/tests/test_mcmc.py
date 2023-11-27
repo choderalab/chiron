07521290be62faf07b28bd5a3e544dee7f67e70c
wiederm <marcus.wieder@gmail.com>
2023-11-27 18:36:04 +0100

    outlining some additional tests

---
+++ chiron/tests/test_mcmc.py
@@ -0,0 +1,8 @@
+def test_sample_from_harmonic_osciallator():
+    pass
+
+def test_sample_from_joint_distribution_of_two_HO():
+    pass
+
+def test_sample_from_joint_distribution_of_two_HO_with_local_moves():
+    pass
