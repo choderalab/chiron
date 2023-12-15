8b5ccb9fbb35bd8c8474d398877d272fbc4dc05b
wiederm <marcus.wieder@gmail.com>
2023-12-15 12:42:38 +0100

    adding fixture for different testsystems

--- chiron/tests/conftest.py
+++ chiron/tests/conftest.py
@@ -10,3 +10,21 @@ def remove_h5_file():
     yield
     if os.path.exists("test.h5"):
         os.remove("test.h5")
+
+
+@pytest.fixture
+def provide_testsystems_and_potentials():
+    from chiron.potential import HarmonicOscillatorPotential, LJPotential
+    from openmmtools.testsystems import HarmonicOscillator, LennardJonesFluid
+
+    ho = HarmonicOscillator()
+    harmonic_potential = HarmonicOscillatorPotential(ho.topology, ho.K, U0=ho.U0)
+
+    lj_fluid = LennardJonesFluid(reduced_density=0.8, n_particles=100)
+    lj_potential = LJPotential(lj_fluid.topology)
+
+    TESTSYSTEM_AND_POTENTIAL = [
+        (ho, harmonic_potential),
+        (lj_fluid, lj_potential),
+    ]
+    return TESTSYSTEM_AND_POTENTIAL
