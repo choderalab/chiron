84a85be1be7a7716608700686608e9a711680bf0
wiederm <marcus.wieder@gmail.com>
2024-01-09 18:46:56 +0100

    Add MBAREstimator class for performing mbar analysis

---
+++ chiron/analysis.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+class MBAREstimator:
+    def __init__(self, N_u: int) -> None:
+        self.mbar_f_k = np.zeros(len(N_u))
+
+    def initialize(self, u_kn: np.ndarray, N_k: np.ndarray):
+        """
+        Perform mbar analysis
+        """
+        from pymbar import MBAR
+        from loguru import logger as log
+
+        log.debug(f"{N_k=}")
+        mbar = MBAR(u_kn=u_kn, N_k=N_k)
+        log.debug(mbar.f_k)
+        self.mbar_f_k = mbar.f_k
--- chiron/analyzer.py
+++
@@ -1,3 +0,0 @@
-class MBAREstimator:
-    def __init__(self) -> None:
-        pass
